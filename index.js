21b566edde9f5beadc90f12e4f576ca973934bf1
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2021-12-30 23:29:17 +0200

    Amd store scrape

---
+++ index.js
@@ -0,0 +1,79 @@
+const puppeteer = require('puppeteer');
+
+const SEARCH_TERM = 'processor';
+// const SEARCH_TERM = 'graphics';
+const PRODUCT_ELEM = 'shop-content';
+const PRODUCT_TITLE = '.shop-title';
+const PRODUCT_LINKS = '.shop-links';
+const PRODUCT_PRICE = '.shop-price';
+const PRODUCT_HREF = '.shop-full-specs-link';
+const PAGE_URL = 'https://www.amd.com/en/direct-buy/fi';
+const USERAGENT =
+  'Mozilla/5.0 (X11; Linux x86_64) ' +
+  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36';
+async function run() {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+  // Site doesn't render if default useragent
+  await page.setUserAgent(USERAGENT);
+  await page.goto(PAGE_URL, {
+    waitUntil: 'networkidle2',
+  });
+  console.log('Page loaded');
+
+  // Get product containing the search term from the page
+  const availableProducts = await page.evaluate(
+      (
+          searchTerm,
+          productElemSelector,
+          productTitleSelector,
+          productLinksSelector,
+          productPriceSelector,
+          productHrefSelector,
+      ) => {
+        const wantedProducts = [];
+        const items = Array.from(
+            document.getElementsByClassName(productElemSelector),
+        );
+
+        items.forEach((elem) => {
+          const productTitle = elem.querySelector(productTitleSelector).innerText;
+          const productLink = elem.querySelector(productLinksSelector).innerText;
+          const productPrice = elem.querySelector(productPriceSelector).innerText;
+          const productHref = elem
+              .querySelector(productHrefSelector)
+              .querySelector('a').href;
+          console.log(productTitle);
+          if (
+            productTitle.toLowerCase().includes(searchTerm) &&
+          productLink.toLowerCase().includes('add')
+          ) {
+            producObject = {
+              title: productTitle,
+              price: productPrice,
+              statusLink: productLink,
+              directLink: productHref,
+            };
+            wantedProducts.push(producObject);
+          }
+        });
+        return wantedProducts;
+      },
+      SEARCH_TERM,
+      PRODUCT_ELEM,
+      PRODUCT_TITLE,
+      PRODUCT_LINKS,
+      PRODUCT_PRICE,
+      PRODUCT_HREF,
+  );
+
+  console.debug(availableProducts);
+  browser.close();
+
+  if (availableProducts.length > 0) {
+    console.log('Available products');
+    // send notification
+  }
+}
+
+run();
