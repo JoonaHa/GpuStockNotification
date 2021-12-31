74d72163cfc84356975056ce50f5aadf29005731
JoonaHa <joona.halonen@helsinki.fi>
2021-12-31 05:19:05 +0200

    Eslint changes

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-require("dotenv").config();
-const puppeteer = require("puppeteer");
-const axios = require("axios");
-const amdAvailability = require("./scrapers/amd");
+require('dotenv').config();
+const puppeteer = require('puppeteer');
+const axios = require('axios');
+const amdAvailability = require('./scrapers/amd');
 
-const AMD_SEARCH_TERM = "processor";
-const TELEGRAM_API_URL = "https://api.telegram.org/bot";
-const TELEGRAM_API_TOKEN = process.env.TELEGRAMBOT_TOKEN;
+const TELEGRAM_API_URL = 'https://api.telegram.org/bot';
+const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_TOKEN;
 const TELEGRAM_CHANNEL_ID = process.env.TELEGRAM_CHANNEL_ID;
 // const SEARCH_TERM = 'graphics';
 async function availability() {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({headless: true});
 
-  amdProducts = await amdAvailability.checkAvailability(
-    browser,
-    AMD_SEARCH_TERM
-  );
+  amdProducts = await amdAvailability.checkAvailability(browser);
   console.debug(amdProducts);
   browser.close();
 
   if (amdProducts.length > 0) {
     // send notification
-    console.log("Sending notifications");
+    console.log('Sending notifications');
     await Promise.all(
-      amdProducts.map(async (item) => {
-        await send_to_telegram(item);
-      })
+        amdProducts.map(async (item) => {
+          await sendToTelegram(item);
+        }),
     );
+    return;
   }
+  console.log('Nothing in stock');
 }
 
-async function send_to_telegram(product) {
-  const apiUrl = TELEGRAM_API_URL + TELEGRAM_API_TOKEN + "/" + "sendMessage";
+async function sendToTelegram(product) {
+  const apiUrl = TELEGRAM_API_URL + TELEGRAM_API_TOKEN + '/' + 'sendMessage';
 
   const payload = {
     chat_id: TELEGRAM_CHANNEL_ID,
-    text: `${product.title}\nPRICE:${product.price}\nProduct now in stock!\n\nBUY: ${product.directLink}`,
+    text:
+      `${product.title}\nPRICE: ${product.price}\n` +
+      `Product now in stock!\n\nBUY: ${product.directLink}`,
     disable_web_page_preview: false,
     disable_notification: false,
     protect_content: true,
   };
 
   axios
-    .post(apiUrl, payload)
-    .then((res) => {
-      console.log(`statusCode: ${res.status}`);
-      console.log(res);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+      .post(apiUrl, payload)
+      .then((res) => {
+        console.log(`statusCode: ${res.status}`);
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 }
 
 availability();
--- scrapers/amd.js
+++ scrapers/amd.js
@@ -1,69 +1,71 @@
-const PRODUCT_ELEM = "shop-content";
-const PRODUCT_TITLE = ".shop-title";
-const PRODUCT_LINKS = ".shop-links";
-const PRODUCT_PRICE = ".shop-price";
-const PRODUCT_HREF = ".shop-full-specs-link";
-const PAGE_URL = "https://www.amd.com/en/direct-buy/fi";
+const SEARCH_TERM = process.env.AMD_SEARCH_TERM;
+const PRODUCT_ELEM = 'shop-content';
+const PRODUCT_TITLE = '.shop-title';
+const PRODUCT_LINKS = '.shop-links';
+const PRODUCT_PRICE = '.shop-price';
+const PRODUCT_HREF = '.shop-full-specs-link';
+const PAGE_URL = 'https://www.amd.com/en/direct-buy/fi';
 const USERAGENT =
-  "Mozilla/5.0 (X11; Linux x86_64) " +
-  "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36";
-async function run(browser, searchTerm) {
+  'Mozilla/5.0 (X11; Linux x86_64) ' +
+  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36';
+async function run(browser) {
   const page = await browser.newPage();
   // Site doesn't render if default useragent
   await page.setUserAgent(USERAGENT);
   await page.goto(PAGE_URL, {
-    waitUntil: "networkidle2",
+    waitUntil: 'networkidle2',
   });
   console.log(`Page ${PAGE_URL} loaded`);
 
   // Get product containing the search term from the page
   const availableProducts = await page.evaluate(
-    (
-      searchTerm,
-      productElemSelector,
-      productTitleSelector,
-      productLinksSelector,
-      productPriceSelector,
-      productHrefSelector
-    ) => {
-      const wantedProducts = [];
-      const items = Array.from(
-        document.getElementsByClassName(productElemSelector)
-      );
+      (
+          SEARCH_TERM,
+          productElemSelector,
+          productTitleSelector,
+          productLinksSelector,
+          productPriceSelector,
+          productHrefSelector,
+      ) => {
+        const wantedProducts = [];
+        const items = Array.from(
+            document.getElementsByClassName(productElemSelector),
+        );
 
-      items.forEach((elem) => {
-        const productTitle = elem.querySelector(productTitleSelector).innerText;
-        const productLink = elem.querySelector(productLinksSelector).innerText;
-        const productPrice = elem.querySelector(productPriceSelector).innerText;
-        const productHref = elem
-          .querySelector(productHrefSelector)
-          .querySelector("a").href;
-        if (
-          productTitle.toLowerCase().includes(searchTerm) &&
-          productLink.toLowerCase().includes("add")
-        ) {
-          producObject = {
-            title: productTitle,
-            price: productPrice,
-            statusLink: productLink,
-            directLink: productHref,
-          };
-          wantedProducts.push(producObject);
-        }
-      });
-      return wantedProducts;
-    },
-    searchTerm,
-    PRODUCT_ELEM,
-    PRODUCT_TITLE,
-    PRODUCT_LINKS,
-    PRODUCT_PRICE,
-    PRODUCT_HREF
+        items.forEach((elem) => {
+          const productTitle = elem.querySelector(productTitleSelector).innerText;
+          const productLink = elem.querySelector(productLinksSelector).innerText;
+          const productPrice = elem.querySelector(productPriceSelector).innerText;
+          const productHref = elem
+              .querySelector(productHrefSelector)
+              .querySelector('a').href;
+          if (
+            productTitle.toLowerCase().includes(SEARCH_TERM) &&
+          productLink.toLowerCase().includes('add')
+          ) {
+            producObject = {
+              title: productTitle,
+              price: productPrice,
+              statusLink: productLink,
+              directLink: productHref,
+            };
+            wantedProducts.push(producObject);
+          }
+        });
+        return wantedProducts;
+      },
+      SEARCH_TERM,
+      PRODUCT_ELEM,
+      PRODUCT_TITLE,
+      PRODUCT_LINKS,
+      PRODUCT_PRICE,
+      PRODUCT_HREF,
   );
 
   if (availableProducts.length > 0) {
     console.log(
-      `Available number of products on ${PAGE_URL}: ${availableProducts.length}`
+        `Available number of products on ${PAGE_URL}: ` +
+        `${availableProducts.length}`,
     );
   }
 
